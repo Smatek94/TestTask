c1e671bf4de47a242cb13d91a10302e464401751
Mateusz <mateusz.skolimowski@toreforge.com>
2018-08-01 09:26:12 +0200

    location updates

--- app/src/main/java/com/skolimowskim/verificationtask/MainActivity.kt
+++ app/src/main/java/com/skolimowskim/verificationtask/MainActivity.kt
@@ -1,24 +1,60 @@
 package com.skolimowskim.verificationtask
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.location.*
+import com.skolimowskim.verificationtask.utils.LogUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button_start.setOnClickListener { onStartClicked() }
-        button_stop.setOnClickListener{ onStopClicked() }
+        button_stop.setOnClickListener { onStopClicked() }
+
+        param_a_input.setText("10")
+        param_b_input.setText("10")
+        param_c_input.setText("10")
     }
 
     private fun onStartClicked() {
         val aParam = param_a_input.text.toString().toInt()
+        val bParam = param_b_input.text.toString().toInt()
+        val cParam = param_c_input.text.toString().toInt()
+
+        startLocalizationUpdates(aParam)
     }
 
     private fun onStopClicked() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
 
+    @SuppressLint("MissingPermission") // app is only to show working on multiple task so I assume user manually granted permissions
+    private fun startLocalizationUpdates(aParam: Int) {
+        locationRequest = LocationRequest().apply {
+            interval = (aParam * 1000).toLong()
+            fastestInterval = (aParam * 1000).toLong()
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                LogUtils.log("Current location : " + locationResult.lastLocation)
+            }
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+                                                   locationCallback,
+                                                   null /* Looper */)
     }
 }
---
+++ app/src/main/java/com/skolimowskim/verificationtask/utils/LogUtils.kt
@@ -0,0 +1,10 @@
+package com.skolimowskim.verificationtask.utils
+
+import android.util.Log
+
+object LogUtils {
+    fun log(msg: String) {
+        Log.e("TestTaskLog", msg)
+    }
+
+}
